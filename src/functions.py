eaf6ec9d472d522c982cc660f5afce033638dbb2
arthur <arthurrochadx@gmail.com>
2024-10-28 09:43:25 -0300

    identificando caracteres

--- src/functions.py
+++ src/functions.py
@@ -1,16 +1,47 @@
+from utils import token_map
+
 def read_java_file(filename):
     with open(filename, 'r') as file:
         current_line = 0
         current_row = 0
+        output = ""
         #iterando sob linha e coluna no arquivo de texto
+        current_word = ""
         for line in file:
             current_line += 1
-            for ch in line:
-                current_row += 1
-                print(ch)
+            current_row = 0
+            print(len(line))
+            while current_row < len(line):
+                ch = line[current_row]
+                current_word += ch
+                if identify_loop(ch):
+                    current_row += 1
+                    ch = line[current_row]
+                    while ch.isalnum():
+                        current_word += ch
+                        current_row += 1
+                        ch = line[current_row]
+                        
+                    print(current_word)
+                        
+                    
+                        
+
+
+                
+
+                
+
 
 
 
 def write_output_file(filename, content):
     with open (filename, 'w') as file:
         file.write(content)
+
+def identify_loop(ch):
+    if ch.isnumeric():
+        return False
+    
+    return True
+        
