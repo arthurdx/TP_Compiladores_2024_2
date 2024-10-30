95d810082282a3faa2b4650c376281d9b1e92231
arthur <arthurrochadx@gmail.com>
2024-10-30 16:17:16 -0300

    verificando os argumentos no terminal

--- src/main.py
+++ src/main.py
@@ -3,7 +3,15 @@ from utils import *
 from functions import *
 import sys
 
-if __name__ == '__main__':
+def main():
+    print(len(sys.argv))
+    if len(sys.argv) < 2:
+        print("Passe o caminho do arquivo txt como argumento no terminal" + 
+        "\nexemplo: python3 src/main.py <txt/arquivo.txt>")
+        return
     filename = sys.argv[1]
     read_java_file(filename)
+
+if __name__ == '__main__':
+    main()
     
