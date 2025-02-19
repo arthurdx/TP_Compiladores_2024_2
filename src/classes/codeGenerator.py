09fb9c97319db288e41c6ef97506b8e709e5222e
arthur <arthurrochadx@gmail.com>
2025-02-19 16:05:10 -0300

    scan com none no 2 operando

--- src/classes/codeGenerator.py
+++ src/classes/codeGenerator.py
@@ -14,26 +14,31 @@ class CodeGenerator:
     def visit(self, node):
         method_name = f'visit_{type(node).__name__}'
         method = getattr(self, method_name, self.generic_visit)
-        method(node)
+        return method(node)
 
     def generic_visit(self, node):
         raise Exception(f'Nenhum método para visitar o nó {type(node).__name__}')
 
     def new_label(self):
-        return f'__label{self.label_count}'
+        label = f'__label{self.label_count}'
+        self.label_count += 1
+        return label
 
     def new_temp(self):
-        return f'__temp{self.temp_count}'
+        temp = f'__temp{self.temp_count}'
+        self.temp_count += 1
+        return temp
 
-    # ---- Métodos Principais Corrigidos ----
+    # ------ Métodos principais corrigidos ------
     def visit_ProgramNode(self, node):
         for func in node.functions:
             self.visit(func)
 
     def visit_FunctionNode(self, node):
         self.current_vars = {}
-        self.visit(node.body)
-
+        for stmt in node.body.statements:
+            self.visit(stmt)
+    
     def visit_BlockNode(self, node):
         for stmt in node.statements:
             self.visit(stmt)
@@ -50,70 +55,79 @@ class CodeGenerator:
                 value = node.types.value.strip('"')
                 self.code.append(('CALL', 'PRINT', value, None))
             else:
-                var_name = self._get_var_name(node.types)
+                var_name = self._resolve_variable(node.types)
                 self.code.append(('CALL', 'PRINT', None, var_name))
         elif node.op == "in":
-            var_name = self._get_var_name(node.values)
-            self.code.append(('CALL', 'SCAN', None, var_name))  # Corrigido: 4º elemento é a variável
+            var_name = self._resolve_variable(node.values)
+            self.code.append(('CALL', 'SCAN', None, var_name))
 
     def visit_WhileNode(self, node):
-        start_label = self.new_label()
-        exit_label = self.new_label()
+        loop_start = self.new_label()
+        loop_exit = self.new_label()
         
-        self.code.append(('LABEL', start_label, None, None))
-        cond_temp = self._process_condition(node.condition)
-        self.code.append(('IF', cond_temp, exit_label, start_label))
+        # Label inicial
+        self.code.append(('LABEL', loop_start, None, None))
         
+        # Processar condição (ex: numBloco > 1)
+        cond_temp = self._process_expression(node.condition)
+        self.code.append(('IF', cond_temp, loop_exit, loop_start))  # Se falso, sai
+        
+        # Corpo do loop
         self.visit(node.body)
-        self.code.append(('JUMP', start_label, None, None))
-        self.code.append(('LABEL', exit_label, None, None))
+        self.code.append(('JUMP', loop_start, None, None))
+        self.code.append(('LABEL', loop_exit, None, None))
 
     def visit_IfNode(self, node):
         true_label = self.new_label()
         false_label = self.new_label()
         end_label = self.new_label()
 
-        cond_temp = self._process_condition(node.condition)
+        cond_temp = self._process_expression(node.condition)
         self.code.append(('IF', cond_temp, true_label, false_label))
 
+        # Bloco verdadeiro
         self.code.append(('LABEL', true_label, None, None))
         self.visit(node.body)
         self.code.append(('JUMP', end_label, None, None))
 
+        # Bloco falso
         self.code.append(('LABEL', false_label, None, None))
         if node.else_body:
             self.visit(node.else_body)
         self.code.append(('LABEL', end_label, None, None))
 
     def visit_AssignmentNode(self, node):
-        target = f"{node.identifier}_0"
-        value = self._process_expression(node.value)
-        self.code.append(('=', target, value, None))
+        target_var = f"{node.identifier}_0"
+        value_temp = self._process_expression(node.value)
+        self.code.append(('=', target_var, value_temp, None))
 
     def visit_BinaryOpNode(self, node):
         left = self._resolve_operand(node.left)
         right = self._resolve_operand(node.right)
         temp = self.new_temp()
         self.code.append((node.op, temp, left, right))
-        return temp
+        return temp  # Retorna o temporário para uso em outras operações
 
-    # ---- Métodos Auxiliares ----
-    def _get_var_name(self, node):
+    # ------ Métodos auxiliares ------
+    def _resolve_variable(self, node):
         if isinstance(node, VariableNode):
             return f"{node.name}_0"
-        elif isinstance(node, str):  # Caso o valor seja passado como string
-            return f"{node}_0"
+        if isinstance(node, LiteralNode):
+            return node.value
         return None
 
-    def _process_condition(self, node):
-        return self.visit(node)
+    def _process_expression(self, expr_node):
+        if isinstance(expr_node, BinaryOpNode):
+            return self.visit_BinaryOpNode(expr_node)
+        elif isinstance(expr_node, VariableNode):
+            return self._resolve_variable(expr_node)
+        elif isinstance(expr_node, LiteralNode):
+            return expr_node.value
+        return None
 
-    def _process_expression(self, node):
-        return self.visit(node)
-
-    def _resolve_operand(self, node):
-        if isinstance(node, VariableNode):
-            return self._get_var_name(node)
-        elif isinstance(node, LiteralNode):
-            return node.value
+    def _resolve_operand(self, operand):
+        if isinstance(operand, VariableNode):
+            return f"{operand.name}_0"
+        elif isinstance(operand, LiteralNode):
+            return operand.value
         return None
